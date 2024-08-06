e951a98b41f7f350f32c50b5edbcb35c8b493923
kayce <kayce@bituniverse.org>
2024-08-06 11:46:44 +0800

    feat(nvim): update markview.nvim config.

--- nvim/lua/plugin-config/markview.lua
+++ nvim/lua/plugin-config/markview.lua
@@ -1,7 +1,42 @@
 vim.api.nvim_set_hl(0, 'MarkviewCodeblock', { bg = 'none' }) -- #2C3144
+vim.api.nvim_set_hl(0, 'MarkviewH1', { fg = '#f38ba8', bg = '#553e53' })
+vim.api.nvim_set_hl(0, 'MarkviewH2', { fg = '#fab387', bg = '#57484b' })
+vim.api.nvim_set_hl(0, 'MarkviewH3', { fg = '#f9e2af', bg = '#575455' })
+vim.api.nvim_set_hl(0, 'MarkviewH4', { fg = '#a6e3a1', bg = '#425451' })
+vim.api.nvim_set_hl(0, 'MarkviewH5', { fg = '#74c7ec', bg = '#364d64' })
+vim.api.nvim_set_hl(0, 'MarkviewH6', { fg = '#b4befe', bg = '#464b68' })
+
 require("markview").setup({
     buf_ignore = { "nofile" },
     modes = { "n", "no" },
+    headings = {
+        enable = true,
+        shift_width = 1,
+        heading_1 = {
+            style = "icon",
+            hl = "MarkviewH1"
+        },
+        heading_2 = {
+            style = "icon",
+            hl = "MarkviewH2"
+        },
+        heading_3 = {
+            style = "icon",
+            hl = "MarkviewH3"
+        },
+        heading_4 = {
+            style = "icon",
+            hl = "MarkviewH4"
+        },
+        heading_5 = {
+            style = "icon",
+            hl = "MarkviewH5"
+        },
+        heading_6 = {
+            style = "icon",
+            hl = "MarkviewH6"
+        },
+    },
     code_blocks = {
         enable = true,
         style = "language",
@@ -20,15 +55,15 @@ require("markview").setup({
         enable = true,
         checked = {
             text = " ",
-            hl = "DiagnosticSignOk"
+            hl = "DiagnosticOk"
         },
         unchecked = {
-            text = " ",
-            hl = "DiagnosticSignError"
+            text = " ",
+            hl = "DiagnosticError"
         },
         pending = {
             text = " ",
-            hl = "DiagnosticSignWarn"
+            hl = "DiagnosticWarn"
         }
     },
     horizontal_rules = {
