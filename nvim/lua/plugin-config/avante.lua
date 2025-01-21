729144b445b8965115ab876365a205f5d4f1ca50
kayce <shaojiale.gitignore@icloud.com>
2025-01-21 14:47:10 +0800

    feat(nvim): use deepseek for avante.

--- nvim/lua/plugin-config/avante.lua
+++ nvim/lua/plugin-config/avante.lua
@@ -1,13 +1,21 @@
 require('avante_lib').load()
 require('avante').setup(
     {
-        provider = "copilot",                  -- Recommend using Claude
-        auto_suggestions_provider = "copilot", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
+        provider = "openai",                  -- Recommend using Claude
+        auto_suggestions_provider = "openai", -- Since auto-suggestions are a high-frequency operation and therefore expensive, it is recommended to specify an inexpensive provider or even a free provider: copilot
         claude = {
             endpoint = "https://api.anthropic.com",
             model = "claude-3-5-sonnet-20241022",
             temperature = 0,
             max_tokens = 4096,
+        },
+        openai = {
+            endpoint = "https://api.deepseek.com/v1",
+            model = "deepseek-chat",
+            timeout = 10000, -- Timeout in milliseconds
+            temperature = 0,
+            max_tokens = 4096,
+            api_key_name = "DEEPSEEK_API_KEY",
         },
         dual_boost = {
             enabled = false,
