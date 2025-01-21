af7602f87cdfd0d1eda29a1ec3ff0dadcb79715d
kayce <shaojiale.gitignore@icloud.com>
2025-01-21 15:55:57 +0800

    feat(nvim): disable auto trigger copilot.

--- nvim/lua/plugin-config/copilot.lua
+++ nvim/lua/plugin-config/copilot.lua
@@ -16,7 +16,7 @@ require('copilot').setup({
     },
     suggestion = {
         enabled = true,
-        auto_trigger = true,
+        auto_trigger = false,
         hide_during_completion = true,
         debounce = 75,
         keymap = {
