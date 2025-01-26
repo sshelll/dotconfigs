dc9f27e6a6dfde9d5a891dd503835c87c830ad5c
kayce <shaojiale.gitignore@icloud.com>
2025-01-26 16:39:42 +0800

    fix(nvim)!: upgrade markview.nvim to 25.x.

--- nvim/lua/plugin-config/markview.lua
+++ nvim/lua/plugin-config/markview.lua
@@ -1,140 +1,144 @@
 -- vim.api.nvim_set_hl(0, 'MarkviewCodeblock', { bg = 'none' }) -- #2C3144
 
 require("markview").setup({
-    buf_ignore = { "nofile" },
-    modes = { "n", "no" },
-    hybrid_modes = { "n", "no" },
+    preview = {
+        buf_ignore = { "nofile" },
+        modes = { "n", "no" },
+        hybrid_modes = { "n", "no" },
+    },
     highlight_groups = {
-        {
-            group_name = "MarkviewH1",
-            value = { fg = "#f38ba8", bg = "#553e53" },
-        },
-        {
-            group_name = "MarkviewH2",
-            value = { fg = "#fab387", bg = "#57484b" },
-        },
-        {
-            group_name = "MarkviewH3",
-            value = { fg = "#f9e2af", bg = "#575455" },
-        },
-        {
-            group_name = "MarkviewH4",
-            value = { fg = "#a6e3a1", bg = "#425451" },
-        },
-        {
-            group_name = "MarkviewH5",
-            value = { fg = "#74c7ec", bg = "#364d64" },
-        },
-        {
-            group_name = "MarkviewH6",
-            value = { fg = "#b4befe", bg = "#464b68" },
-        },
-        {
-            group_name = "MarkviewCodeblock",
-            value = { bg = "none" },
-        }
+        MarkviewH1 = { fg = "#f38ba8", bg = "#553e53" },
+        MarkviewH2 = { fg = "#fab387", bg = "#57484b" },
+        MarkviewH3 = { fg = "#f9e2af", bg = "#575455" },
+        MarkviewH4 = { fg = "#a6e3a1", bg = "#425451" },
+        MarkviewH5 = { fg = "#74c7ec", bg = "#364d64" },
+        MarkviewH6 = { fg = "#b4befe", bg = "#464b68" },
+        MarkviewIcon0 = { fg = "#555f88", bg = "none" },
+        MarkviewIcon1 = { fg = "#79a2f6", bg = "none" },
+        MarkviewIcon2 = { fg = "#dfaf67", bg = "none" },
+        MarkviewIcon3 = { fg = "#9dce69", bg = "none" },
+        MarkviewIcon4 = { fg = "#19bc9b", bg = "none" },
+        MarkviewIcon5 = { fg = "#ba9af6", bg = "none" },
+        MarkviewIcon6 = { fg = "#9c7cd7", bg = "none" },
+        MarkviewCodeblock = { bg = "none" },
+        MarkviewInlineCode = { bg = "none" },
     },
-    headings = {
+    markdown = {
         enable = true,
-        shift_width = 1,
-        heading_1 = {
-            style = "icon",
-            hl = "MarkviewH1"
-        },
-        heading_2 = {
-            style = "icon",
-            hl = "MarkviewH2"
-        },
-        heading_3 = {
-            style = "icon",
-            hl = "MarkviewH3"
-        },
-        heading_4 = {
-            style = "icon",
-            hl = "MarkviewH4"
-        },
-        heading_5 = {
-            style = "icon",
-            hl = "MarkviewH5"
-        },
-        heading_6 = {
-            style = "icon",
-            hl = "MarkviewH6"
-        },
-    },
-    code_blocks = {
-        enable = true,
-        style = "language",
-        position = "overlay",
-
-        hl = "MarkviewCodeblock",
-
-        min_width = 100,
-        pad_char = " ",
-        pad_amount = 2,
-
-        language_direction = "right",
-        sign = true,
-    },
-    checkboxes = {
-        enable = true,
-        checked = {
-            text = " ",
-            hl = "DiagnosticOk"
-        },
-        unchecked = {
-            text = " ",
-            hl = "DiagnosticError"
-        },
-        pending = {
-            text = " ",
-            hl = "DiagnosticWarn"
-        }
-    },
-    horizontal_rules = {
-        enable = true,
-
-        position = "overlay",
-        parts = {
-            {
-                type = "repeating",
-                repeat_amount = function() --[[@as function]]
-                    local textoff = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1].textoff;
-
-                    return math.floor((vim.o.columns - textoff - 3) / 2);
-                end,
-
-                text = "─",
-                hl = {
-                    "MarkviewGradient1", "MarkviewGradient2", "MarkviewGradient3", "MarkviewGradient4",
-                    "MarkviewGradient5", "MarkviewGradient6", "MarkviewGradient7", "MarkviewGradient8",
-                    "MarkviewGradient9", "MarkviewGradient10"
-                }
+        tables = require("markview.presets").tables.rounded,
+        headings = {
+            enable = true,
+            shift_width = 1,
+            heading_1 = {
+                style = "icon",
+                hl = "MarkviewH1"
             },
-            {
-                type = "repeating",
-                repeat_amount = function() --[[@as function]]
-                    local textoff = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1].textoff;
+            heading_2 = {
+                style = "icon",
+                hl = "MarkviewH2"
+            },
+            heading_3 = {
+                style = "icon",
+                hl = "MarkviewH3"
+            },
+            heading_4 = {
+                style = "icon",
+                hl = "MarkviewH4"
+            },
+            heading_5 = {
+                style = "icon",
+                hl = "MarkviewH5"
+            },
+            heading_6 = {
+                style = "icon",
+                hl = "MarkviewH6"
+            },
+        },
+        horizontal_rules = {
+            enable = true,
+            position = "overlay",
+            parts = {
+                {
+                    type = "repeating",
+                    repeat_amount = function() --[[@as function]]
+                        local textoff = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1].textoff;
 
-                    return math.ceil((vim.o.columns - textoff - 3) / 2);
-                end,
+                        return math.floor((vim.o.columns - textoff - 3) / 2);
+                    end,
+                    text = "─",
+                    hl = {
+                        "MarkviewGradient1", "MarkviewGradient2", "MarkviewGradient3", "MarkviewGradient4",
+                        "MarkviewGradient5", "MarkviewGradient6", "MarkviewGradient7", "MarkviewGradient8",
+                        "MarkviewGradient9", "MarkviewGradient10"
+                    }
+                },
+                {
+                    type = "repeating",
+                    repeat_amount = function() --[[@as function]]
+                        local textoff = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1].textoff;
 
-                direction = "right",
-                text = "─",
-                hl = {
-                    "MarkviewGradient1", "MarkviewGradient2", "MarkviewGradient3", "MarkviewGradient4",
-                    "MarkviewGradient5", "MarkviewGradient6", "MarkviewGradient7", "MarkviewGradient8",
-                    "MarkviewGradient9", "MarkviewGradient10"
+                        return math.ceil((vim.o.columns - textoff - 3) / 2);
+                    end,
+
+                    direction = "right",
+                    text = "─",
+                    hl = {
+                        "MarkviewGradient1", "MarkviewGradient2", "MarkviewGradient3", "MarkviewGradient4",
+                        "MarkviewGradient5", "MarkviewGradient6", "MarkviewGradient7", "MarkviewGradient8",
+                        "MarkviewGradient9", "MarkviewGradient10"
+                    }
                 }
             }
+        },
+        code_blocks = {
+            enable = true,
+            style = "language",
+            position = "overlay",
+
+            border_hl = "MarkviewCodeblock",
+            default = {
+                block_hl = "MarkviewCodeblock",
+                pad_hl = "MarkviewCodeblock"
+            },
+
+            min_width = 100,
+            pad_char = " ",
+            pad_amount = 2,
+
+            language_direction = "right",
+            sign = true,
+        },
+        block_quotes = {
+            enable = true,
+
+            default = {
+                border = "▋",
+            },
+            callouts = nil
+        },
+        metadata_minus = {
+            enable = true,
+            hl = "MarkviewCodeblock",
+            border_hl = "MarkviewCodeblock",
+            border_top = "",
+            border_bottom = ""
         }
     },
-    block_quotes = {
-        enable = true,
-
-        default = {
-            border = "▋",
+    markdown_inline = {
+        checkboxes = {
+            enable = true,
+            checked = {
+                text = " ",
+                hl = "DiagnosticOk"
+            },
+            unchecked = {
+                text = " ",
+                hl = "DiagnosticError"
+            },
+            pending = {
+                text = " ",
+                hl = "DiagnosticWarn"
+            }
         },
-        callouts = nil
     }
 });
