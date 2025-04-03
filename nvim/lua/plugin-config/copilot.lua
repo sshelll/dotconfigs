41588f2586fb44fc784bbe2d7fff6e64114b0f45
kayce <shaojiale.gitignore@icloud.com>
2025-04-03 16:01:30 +0800

    refactor(nvim): simplify copilot config.

--- nvim/lua/plugin-config/copilot.lua
+++ nvim/lua/plugin-config/copilot.lua
@@ -1,19 +1,4 @@
 require('copilot').setup({
-    panel = {
-        enabled = true,
-        auto_refresh = false,
-        keymap = {
-            jump_prev = "[[",
-            jump_next = "]]",
-            accept = "<CR>",
-            refresh = "gr",
-            open = "<M-CR>"
-        },
-        layout = {
-            position = "bottom", -- | top | left | right | horizontal | vertical
-            ratio = 0.4
-        },
-    },
     suggestion = {
         enabled = true,
         auto_trigger = false,
@@ -39,6 +24,4 @@ require('copilot').setup({
         cvs = false,
         ["."] = false,
     },
-    copilot_node_command = 'node', -- Node.js version must be > 18.x
-    server_opts_overrides = {},
 })
