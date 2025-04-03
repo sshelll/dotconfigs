363f81e148b346a75c380feec5dc039a26011b23
kayce <shaojiale.gitignore@icloud.com>
2025-04-03 16:01:48 +0800

    feat(nvim): update noice settings.

--- nvim/lua/plugin-config/noice.lua
+++ nvim/lua/plugin-config/noice.lua
@@ -1,10 +1,14 @@
 require("noice").setup({
-    -- cmdline = {
-    --     view = "cmdline_popup",
-    -- },
+    presets = {
+        -- bottom_search = true,         -- use a classic bottom cmdline for search
+        command_palette = true,       -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true,        -- add a border to hover docs and signature help
+    },
     popupmenu = {
         enabled = true,
-        backend = "cmp",
+        backend = "nui",
     },
     views = {
         cmdline_popup = {
