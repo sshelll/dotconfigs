6f9d31a4c27d5bc0168a2bcb9b98725406442181
kayce <shaojiale.gitignore@icloud.com>
2025-06-05 17:46:12 +0800

    fix(nvim): adjust avante config.

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -31,13 +31,13 @@ local pluglist = {
         version = false, -- set this if you want to always pull the latest change
         opts = {
             provider = "deepseek",
-            vendors = {
+            providers = {
                 deepseek = {
                     __inherited_from = "openai",
                     api_key_name = "DEEPSEEK_API_KEY",
                     endpoint = "https://api.deepseek.com",
                     model = "deepseek-coder",
-                    max_tokens = 4096,
+                    max_tokens = 8192,
                 },
             },
         },
