f9dbb0ce9703e83b6d61ad4623b4754359ff6f1f
kayce <shaojiale.gitignore@icloud.com>
2025-10-16 13:43:24 +0800

    chore(nvim): upgrade plugins.

--- nvim/lua/plugin-config/copilot.lua
+++ nvim/lua/plugin-config/copilot.lua
@@ -1,27 +1,27 @@
-require('copilot').setup({
-    suggestion = {
-        enabled = true,
-        auto_trigger = false,
-        hide_during_completion = true,
-        debounce = 75,
-        keymap = {
-            accept = "<C-c>",
-            accept_word = false,
-            accept_line = false,
-            next = "∆", -- <M-j> on macOS
-            prev = "˚", -- <M-k> on macOS
-            dismiss = "˙", -- <M-h> on macOS
-        },
-    },
-    filetypes = {
-        yaml = true,
-        markdown = true,
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ["."] = false,
-    },
+require("copilot").setup({
+	suggestion = {
+		enabled = true,
+		auto_trigger = false,
+		hide_during_completion = true,
+		debounce = 75,
+		keymap = {
+			accept = "<C-c>",
+			accept_word = false,
+			accept_line = false,
+			next = "∆", -- <M-j> on macOS
+			prev = "˚", -- <M-k> on macOS
+			dismiss = "˙", -- <M-h> on macOS
+		},
+	},
+	filetypes = {
+		yaml = true,
+		markdown = true,
+		help = false,
+		gitcommit = false,
+		gitrebase = false,
+		hgcommit = false,
+		svn = false,
+		cvs = false,
+		["."] = false,
+	},
 })
